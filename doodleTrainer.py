fc1a7b9443310d9a7a2229993b8f1d639af10dd7
mchrpt <mchrpt@yahoo.com>
2023-11-17 15:53:51 -0500

    Refactor for cleaner and more readable code.

--- doodleTrainer.py
+++ doodleTrainer.py
@@ -7,12 +7,16 @@ import time
 import random
 import keyboard
 
+
 def initWindow():
-    win = pygetwindow.getWindowsWithTitle('Toontown Rewritten')[0]
+    """Grabs and resizes the window of the game"""
+    win = pygetwindow.getWindowsWithTitle('Toontown Rewritten')[0]  # TODO: Add Corporate Clash functionality
     win.resizeTo(800, 600)
     win.moveTo(0, 0)
 
+
 def getCoordinates(screen, image):
+    """Gets the position of a specified image on the screen using cv2 to match it"""
     w, h = image.shape[:-1]
 
     res = cv2.matchTemplate(screen, image, cv2.TM_CCOEFF_NORMED)
@@ -30,37 +34,37 @@ def getCoordinates(screen, image):
     if count > 0:
         avg_x = avg_x / count
         avg_y = avg_y / count
-        
-    return avg_x + (h/2), avg_y + (w/2)
+
+    return avg_x + (h / 2), avg_y + (w / 2)
+
 
 def calibrate():
-    print('Starting Calibration')
-    screen = np.array(ImageGrab.grab(bbox=(0, 0, 400, 400)))
+    """Determines the coordinates of each of the tricks on the screen"""
+    displayMessage('Starting Calibration... please wait...')
     current_mouse_pos = pyautogui.position()
 
     pictures = {}
-    speedchat = cv2.imread('pictures/speedchat.png')
-    pets = cv2.imread('pictures/pets.png')
-    tricks = cv2.imread('pictures/tricks.png')
-    pictures['backflip'] = cv2.imread('pictures/backflip.png')
-    pictures['beg'] = cv2.imread('pictures/beg.png')
-    pictures['dance'] = cv2.imread('pictures/dance.png')
-    pictures['jump'] = cv2.imread('pictures/jump.png')
-    pictures['play_dead'] = cv2.imread('pictures/play_dead.png')
-    pictures['rollover'] = cv2.imread('pictures/rollover.png')
-    pictures['speak'] = cv2.imread('pictures/speak.png')
+    speedchat = cv2.imread('Image Data/speedchat.png')
+    pets = cv2.imread('Image Data/pets.png')
+    tricks = cv2.imread('Image Data/tricks.png')
+    pictures['backflip'] = cv2.imread('Image Data/backflip.png')
+    pictures['beg'] = cv2.imread('Image Data/beg.png')
+    pictures['dance'] = cv2.imread('Image Data/dance.png')
+    pictures['jump'] = cv2.imread('Image Data/jump.png')
+    pictures['play_dead'] = cv2.imread('Image Data/play_dead.png')
+    pictures['rollover'] = cv2.imread('Image Data/rollover.png')
+    pictures['speak'] = cv2.imread('Image Data/speak.png')
 
-    coordinates = {}
-    coordinates['speedchat'] = getCoordinates(screen=np.array(ImageGrab.grab(bbox=(0, 0, 400, 400))), image=speedchat)
+    coordinates = {'speedchat': getCoordinates(screen=np.array(ImageGrab.grab(bbox=(0, 0, 400, 400))), image=speedchat)}
     pyautogui.click(coordinates['speedchat'])
-    
+
     coordinates['pets'] = getCoordinates(screen=np.array(ImageGrab.grab(bbox=(0, 0, 400, 400))), image=pets)
     pyautogui.moveTo(coordinates['pets'])
 
     coordinates['tricks'] = getCoordinates(screen=np.array(ImageGrab.grab(bbox=(0, 0, 400, 400))), image=tricks)
     pyautogui.moveTo(coordinates['tricks'][0] - 15, coordinates['tricks'][1])
     coordinates['here_boy'] = [coordinates['tricks'][0], coordinates['tricks'][1] + 15]
-    
+
     screen = np.array(ImageGrab.grab(bbox=(0, 0, 400, 400)))
     tricks = []
     for key in pictures.keys():
@@ -71,55 +75,66 @@ def calibrate():
 
     pyautogui.click(coordinates['speedchat'])
     pyautogui.click(current_mouse_pos)
-            
+
     if len(coordinates) > 5:
-        print('Calibration Successful')
+        displayMessage('Calibration Successful')
         return coordinates, tricks
     else:
-        print('Calibration Failed')
+        displayMessage('Calibration Failed')
         return None, None
-    
+
+
 def performTrick(coordinates, selected_tricks):
+    """Navigates the mouse to the specified trick via dropdowns and predetermined coordinates"""
     trick = random.randint(0, len(selected_tricks) - 1)
     pyautogui.click(coordinates['speedchat'])
     pyautogui.moveTo(coordinates['pets'])
     pyautogui.moveTo(coordinates['tricks'])
     pyautogui.click(coordinates[selected_tricks[trick]])
 
+
 def hereBoy(coordinates):
+    """Navigates the mouse to call the doodle back to us if it runs too far away"""
     pyautogui.click(coordinates['speedchat'])
     pyautogui.moveTo(coordinates['pets'])
     pyautogui.click(coordinates['here_boy'])
 
-def start():
+
+def takeUserInput():
+    """Navigates the mouse to call the doodle back to us if it runs too far away"""
+    displayMessage('Select the tricks you want to perform with commas separating the numbers:')
+    trick_message = ""
+    for i in range(len(tricks)):
+        trick_message += f'{i + 1}: {tricks[i]}'
+    print(end='>')
+    selected = list(map(int, input().split(',')))
+    displayMessage('How many hours (4-8 is recommended) would you like to run the trainer:')
+    print(end='>')
+    hours = float(input())
+    for i in range(len(selected)):
+        selected[i] -= 1
+
+    selected_tricks = [tricks[num] for num in selected]
+    return hours, selected_tricks
+
+
+def displayMessage(message):
+    """Input a string and have it wrapped nicely"""
+    print('=========================================================================')
+    print(message)
+    print('=========================================================================')
+
+
+if __name__ == "__main__":
     initWindow()
     coordinates, tricks = calibrate()
-    if not coordinates == None:
+
+    if coordinates is not None:
         start_time = time.time()
-
-        print('=========================================================================')
-        print('Select the tricks you want to perform with commas seperating the numbers:')
-        print('=========================================================================')
-        for i in range(len(tricks)):
-            print(f'{i + 1}: {tricks[i]}')
-
-        print('=========================================================================')
-        print(end='>')
-        selected = list(map(int, input().split(',')))
-        print('=========================================================================')
-        print('How many hours would you like to run the trainer:')
-        print('=========================================================================')
-        print(end='>')
-        hours = float(input())
-        print('=========================================================================')
-        
-        for i in range(len(selected)):
-            selected[i] -= 1
-        
-        selected_tricks = [tricks[num] for num in selected]
+        hours, selected_tricks = takeUserInput()
         tricks_performed = 0
-
-        while(hours > (time.time() - start_time) / 3600):
+        displayMessage('Starting training, you can press the escape key to cancel at any time')
+        while hours > (time.time() - start_time) / 3600:
             if keyboard.is_pressed("ESC"):
                 break
 
@@ -128,12 +143,10 @@ def start():
                 hereBoy(coordinates)
                 time.sleep(1 + (rand_variation * 2) / 100)
 
-            print(f'Hours elapsed: {round((time.time() - start_time) / 3600, 2)}/{hours}')
-            print(f'Tricks attempted: {tricks_performed}')
-            print('=========================================================================')
+            hours_message = f'Hours elapsed: {round((time.time() - start_time) / 3600, 2)}/{hours}'
+            tricks_message = f'Tricks attempted: {tricks_performed}'
+            displayMessage(hours_message + "\n" + tricks_message)
+
             performTrick(coordinates, selected_tricks)
             tricks_performed += 1
-            time.sleep(3.5 + random.randrange(0,4))
-
-if __name__ == "__main__":
-   start()
+            time.sleep(3.5 + random.randrange(0, 4))
